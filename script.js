47af150fcec52d10e614308fb8fbbc94d154fa58
Surya Pratap Singh <suryaprataps424@gmail.com>
2023-12-03 22:07:39 +0530

    sp

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const images = document.querySelectorAll(".hero-image");
+    let index = 0;
+    
+    function showNextImage() {
+        // Hide all images
+        images.forEach(img => img.style.opacity = "0");
+        // Show the current image
+        images[index].style.opacity = "1";
+        // Move to the next image
+        index = (index + 1) % images.length;
+        // Delay before showing the next image (adjust as needed)
+        setTimeout(showNextImage, 2000);  // Adjust the delay (in milliseconds) between images
+    }
+
+    // Initial call to start the sequence
+    showNextImage();
+});
