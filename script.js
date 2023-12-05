d64b09cff300d2e4300c0b965bc74965c40ed4a3
Surya Pratap Singh <suryaprataps424@gmail.com>
2023-12-05 13:26:07 +0530

    some

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
     const images = document.querySelectorAll(".hero-image");
-    let index = 0;
+    const market_images = document.querySelectorAll(".market-images");
+    let index = 0,i=0;
     
     function showNextImage() {
         // Hide all images
         images.forEach(img => img.style.opacity = "0");
+        market_images.forEach(img => img.style.opacity = "0");
         // Show the current image
         images[index].style.opacity = "1";
+        market_images[i].style.opacity = "1";
         // Move to the next image
         index = (index + 1) % images.length;
+        i = (i + 1) % market_images.length;
         // Delay before showing the next image (adjust as needed)
         // if(index==2){
         //     setTimeout(showNextImage, 1000);  // Adjust the delay (in milliseconds) between images
@@ -26,8 +30,8 @@ let currentIndex = 0;
 function changeCard(direction) {
   const cardContainer = document.querySelector('.cards');
   const cards = document.querySelectorAll('.card');
-  const cardWidth = cards[0].offsetWidth +cards[0].currentStyle.marginRight;
-  console.log(cardWidth);
+  let style = cards[0].currentStyle || window.getComputedStyle(cards[0]);;
+  const cardWidth = cards[0].offsetWidth +2*parseInt(style.marginRight,10);
   currentIndex = currentIndex + direction;
 
   if (currentIndex < 0) {
